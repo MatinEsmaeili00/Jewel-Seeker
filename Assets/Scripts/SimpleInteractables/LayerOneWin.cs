3d48f807c0e05b6b61c93eb6fd0ffbe9519166e8
Matin Esmaeili <mn.matinesmaeili@gmail.com>
2026-05-05 06:25:54 -0500

    added the sounds

--- Assets/Scripts/SimpleInteractables/LayerOneWin.cs
+++ Assets/Scripts/SimpleInteractables/LayerOneWin.cs
@@ -5,14 +5,34 @@ public class LayerOneWin : MonoBehaviour
     private LoadLevels loader;
     public static bool isReadyToTransition;
     
+    public AudioClip winSound;
+
+    private AudioSource cameraAudioSource;
+    
     public static event System.Action OnWeaponCheck;
     void Awake()
     {
         loader = GetComponent<LoadLevels>();
+        
+        Camera currentCamera = Camera.main;
+
+        if (currentCamera == null)
+        {
+            Debug.LogError("No Main Camera found! Make sure your camera has the tag MainCamera.");
+            return;
+        }
+
+        cameraAudioSource = currentCamera.GetComponent<AudioSource>();
+
+        if (cameraAudioSource == null)
+        {
+            Debug.LogError("Main Camera does not have an AudioSource component.");
+            return;
+        }
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        
+        PlayWinSound();
         OnWeaponCheck?.Invoke();
         if (!other.gameObject.CompareTag("Player"))
         {
@@ -26,4 +46,11 @@ public class LayerOneWin : MonoBehaviour
         }
     }
     //hi
+    
+    
+    public void PlayWinSound()
+    {
+        cameraAudioSource.clip = winSound;
+        cameraAudioSource.Play();
+    }
 }
