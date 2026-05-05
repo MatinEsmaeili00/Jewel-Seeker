db47bfacc78311064d2f323fee134d84f0c9d524
Matin Esmaeili <mn.matinesmaeili@gmail.com>
2026-05-04 20:43:53 -0500

    menu transition
    Made transitions for Menu start and end

---
+++ Assets/Scripts/SceneUI/LoadLevels.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevels : MonoBehaviour
+{
+    public void LoadLevel_1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void LoadLevel_2()
+    {
+        SceneManager.LoadScene(2);        
+    }
+
+    public void LoadLevel_3()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
